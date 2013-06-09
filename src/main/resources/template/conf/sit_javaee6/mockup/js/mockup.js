e00f68a97d21a145c07aefc9c901b3e45eb50900
yuichi.kuwahara <>
2013-06-10 07:21:48 +0900

    mockupのtooltipをjquery uiのtooltipに変更

--- src/main/resources/template/conf/sit_javaee6/mockup/js/mockup.js
+++ src/main/resources/template/conf/sit_javaee6/mockup/js/mockup.js
@@ -1,45 +1,33 @@
 $(function() {
-	var num = 1;
-	
-		
-	var showHoverBox = function (jqo) {
-		var hoverBoxStyle = function(jqo) {
-			return "left:" + (jqo.position().left - 4) + "px;" +
-			";top:" + (jqo.position().top - 4) + "px;" +
-			"width:" + (jqo.outerWidth() + 4 ) + "px;" + 
-			"height:" + (jqo.outerHeight() + 4) + "px;";
-		};
-		var style = hoverBoxStyle(jqo);
-		$("body").append("<div class='shownum-hover-box' style='" + style + "'><em class='shownum'>(" + num++ + ")</em></div>");
-
-		$("#header").click(function() {
-			$("div.shownum-hover-box").toggle();
-		});
-
-	};
-	
-	var showWrapBox = function(jqo) {
-		jqo.wrap("<span class='shownum-wrap-box'></span>").after("<em class='shownum'>(" + num++ + ")</em>");
-	};
-	
-	$(".shownum").each(function() {
-//		showHoverBox($(this));
-		showWrapBox($(this));
-	});
-	
-	$("#header").click(function() {
-		$("span.shownum-wrap-box").toggleClass("shownum-wrap-box-off");
-		$("em.shownum").toggle();
-	});
-	
-	$("#header").click();
-
 	
 	$("table.datatable").each(function() {
 		var tbody = $(this).find("tbody");
 		for(var i = 0; i < 2; i++) {
-			tbody.append(tbody.find("tr").clone());
+			var newTr = tbody.find("tr").clone();
+			tbody.append(newTr);
+			newTr.find(".documented").removeClass("documented");
 		}
 		
 	});
+
+	var num = 1;
+
+	$("#logo").css("cursor", "pointer")
+		.click(function() {
+			if (num == 1) {
+				$(".documented").each(function() {
+					if ($(this).attr("id").match(/.*[2-9]$/)) {
+						return;
+					}
+					$(this).attr("title", "(" + num++ + ")")
+						.tooltip({
+							position : {my: "left top", at: "right bottom" },
+							tooltipClass : "numberingTooltip"
+						})
+						.tooltip("open");
+				});
+			} else {
+				$(".numberingTooltip").toggle();
+			}
+		});
 });
